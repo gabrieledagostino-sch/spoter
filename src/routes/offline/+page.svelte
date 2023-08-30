3e078c8a54c12412af4001901210f8697be2e575
gabrieledagostino-sch <gabrieledagostino50@gmail.com>
2023-08-30 15:34:43 +0200

    track isolation

---
+++ src/routes/offline/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Button from "../../components/shared/Button.svelte";
+
+</script>
+<div
+    class="
+        flex
+        flex-1
+        justify-center
+        items-center
+        flex-col
+    "
+>
+    <h1>You are offline</h1>
+    <p>Please make sure you are online and then reload</p>
+    <Button kind={"primary"} submit={false} on:click={window.location.href = '/'}><span>Reload <i class="fa-solid fa-rotate-right"></i></span></Button>
+</div>
--- src/service-worker.js
+++ src/service-worker.js
@@ -5,8 +5,8 @@ import { build, files, version } from '$service-worker';
 const CACHE = `cache-${version}`;
  
 const ASSETS = [
-  ...build, // the app itself
-  ...files  // everything in `static`
+  ...files,  // everything in `static`
+  '/offline'
 ];
  
 self.addEventListener('install', (event) => {
@@ -28,35 +28,4 @@ self.addEventListener('activate', (event) => {
   }
  
   event.waitUntil(deleteOldCaches());
-});
- 
-self.addEventListener('fetch', (event) => {
-  // ignore POST requests etc
-  if (event.request.method !== 'GET') return;
- 
-  async function respond() {
-    const url = new URL(event.request.url);
-    const cache = await caches.open(CACHE);
- 
-    // `build`/`files` can always be served from the cache
-    if (ASSETS.includes(url.pathname)) {
-      return cache.match(url.pathname);
-    }
- 
-    // for everything else, try the network first, but
-    // fall back to the cache if we're offline
-    try {
-      const response = await fetch(event.request);
- 
-      if (response.status === 200) {
-        cache.put(event.request, response.clone());
-      }
- 
-      return response;
-    } catch {
-      return cache.match(event.request);
-    }
-  }
- 
-  event.respondWith(respond());
 });
