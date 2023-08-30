04e605bad13d2792ada0f775efded26ea04090ee
gabrieledagostino-sch <gabrieledagostino50@gmail.com>
2023-08-30 15:51:10 +0200

    service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,31 +1,56 @@
 /// <reference types="@sveltejs/kit" />
-import { build, files, version } from '$service-worker';
+import { files, version } from '$service-worker';
  
 // Create a unique cache name for this deployment
 const CACHE = `cache-${version}`;
  
 const ASSETS = [
-  ...files,  // everything in `static`
-  '/offline'
+    ...files,  // everything in `static`
+    '/offline'
 ];
  
 self.addEventListener('install', (event) => {
   // Create a new cache and add all files to it
-  async function addFilesToCache() {
-    const cache = await caches.open(CACHE);
-    await cache.addAll(ASSETS);
-  }
- 
-  event.waitUntil(addFilesToCache());
+    async function addFilesToCache() {
+        const cache = await caches.open(CACHE);
+        await cache.addAll(ASSETS);
+    }
+    
+    event.waitUntil(addFilesToCache());
 });
  
 self.addEventListener('activate', (event) => {
   // Remove previous cached data from disk
-  async function deleteOldCaches() {
-    for (const key of await caches.keys()) {
-      if (key !== CACHE) await caches.delete(key);
+    async function deleteOldCaches() {
+        for (const key of await caches.keys()) {
+            if (key !== CACHE) await caches.delete(key);
+        }
     }
-  }
  
-  event.waitUntil(deleteOldCaches());
+    event.waitUntil(deleteOldCaches());
 });
+
+self.addEventListener('fetch', (event) => {
+    async function respond() {
+        const cache = await caches.open(CACHE)
+
+        //stale-while-revalidate for static files
+        if(files.includes(event.request.url)) {
+            return cache.match(event.request)
+                .then(cached => {
+                    const fetched = fetch(event.request).then(network => {
+                        cache.put(event.request, network.clone())
+                        return network
+                    })
+                    return cached || fetched
+                })
+        }
+        //Network first fallback offline page
+        return fetch(event.request)
+        .catch(() => {
+            return CACHE.match('/offline')
+        })
+    }
+
+    event.waitUntil(respond())
+})
